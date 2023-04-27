7ed577d190388413ba149efb754cd6f586abdec5
Matheus C. França <matheus-catarino@hotmail.com>
2023-04-27 16:06:34 -0300

    zig-pkg build

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const fs = std.fs;
-const Builder = std.build.Builder;
+const Builder = std.Build;
 
 const debugging = true;
 
@@ -33,45 +33,6 @@ fn create_bpf_prog(ctx: *const Ctx, src_path: ?[]const u8) *std.build.CompileSte
     prog.step.dependOn(ctx.mounting_step);
 
     return prog;
-}
-
-fn create_libbpf(b: *Builder, target: std.zig.CrossTarget, optimize: std.builtin.Mode) *std.build.CompileStep {
-    const libbpf = b.addStaticLibrary(.{
-        .name = "libbpf",
-        .target = target,
-        .optimize = optimize,
-    });
-    const libbpfSources = [_][]const u8{
-        "external/libbpf/src/bpf.c",
-        "external/libbpf/src/btf.c",
-        "external/libbpf/src/libbpf.c",
-        "external/libbpf/src/libbpf_errno.c",
-        "external/libbpf/src/netlink.c",
-        "external/libbpf/src/nlattr.c",
-        "external/libbpf/src/str_error.c",
-        "external/libbpf/src/libbpf_probes.c",
-        "external/libbpf/src/bpf_prog_linfo.c",
-        "external/libbpf/src/btf_dump.c",
-        "external/libbpf/src/hashmap.c",
-        "external/libbpf/src/ringbuf.c",
-        "external/libbpf/src/strset.c",
-        "external/libbpf/src/linker.c",
-        "external/libbpf/src/gen_loader.c",
-        "external/libbpf/src/relo_core.c",
-        "external/libbpf/src/usdt.c",
-    };
-    const libbpfFlags = [_][]const u8{
-        "-D_LARGEFILE64_SOURCE",
-        "-D_FILE_OFFSET_BITS=64",
-    };
-    libbpf.addCSourceFiles(&libbpfSources, &libbpfFlags);
-    libbpf.addIncludePath("external/libbpf/include");
-    libbpf.addIncludePath("external/libbpf/include/uapi");
-    libbpf.linkLibC();
-    libbpf.linkSystemLibrary("elf");
-    libbpf.linkSystemLibrary("z");
-
-    return libbpf;
 }
 
 fn create_mounting_tracefs_step(b: *Builder) *Builder.Step {
@@ -111,7 +72,11 @@ pub fn build(b: *Builder) !void {
     const mounting_step = create_mounting_tracefs_step(b);
 
     // build libbpf
-    const libbpf = create_libbpf(b, target, optimize);
+    const libbpf_dep = b.dependency("libbpf", .{
+        .optimize = optimize,
+        .target = target,
+    });
+    const libbpf = libbpf_dep.artifact("bpf");
 
     // build bpf package
     const bpf = b.addModule("bpf", .{
@@ -142,6 +107,7 @@ pub fn build(b: *Builder) !void {
         .source_file = prog.getOutputSource(),
     });
 
+    exe.installLibraryHeaders(libbpf);
     exe.linkLibrary(libbpf);
     exe.linkLibC();
     exe.addIncludePath("external/libbpf/src");
