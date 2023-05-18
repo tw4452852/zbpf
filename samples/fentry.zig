c4e50e75ab3eb83c7c20bc4d559696d86561e6ef
Tw <tw19881113@gmail.com>
2023-05-18 10:03:33 +0800

    fentry: split entry and exit test
    Signed-off-by: Tw <tw19881113@gmail.com>

--- samples/fentry.zig
+++ samples/fentry.zig
@@ -1,20 +1,17 @@
 const std = @import("std");
 const bpf = @import("bpf");
 
-var ret = bpf.Map.ArrayMap("ret", usize, 1, 0).init();
+var entry = bpf.Map.ArrayMap("entry", u64, 1, 0).init();
+var exit = bpf.Map.ArrayMap("exit", i64, 1, 0).init();
 
 const tracked_func = bpf.Fentry{ .name = "path_listxattr" };
 
-var n: usize = 0;
-
-export fn fentry(args: *tracked_func.Ctx()) linksection(tracked_func.entry_section()) callconv(.C) c_long {
-    n +|= args.arg2;
-    ret.update(.any, 0, n) catch {};
+export fn test_fentry(args: *tracked_func.Ctx()) linksection(tracked_func.entry_section()) callconv(.C) c_long {
+    entry.update(.any, 0, args.arg2) catch {};
     return 0;
 }
 
-export fn fexit(args: *tracked_func.Ctx()) linksection(tracked_func.exit_section()) callconv(.C) c_long {
-    n +|= @intCast(usize, args.ret);
-    ret.update(.any, 0, n) catch {};
+export fn test_fexit(args: *tracked_func.Ctx()) linksection(tracked_func.exit_section()) callconv(.C) c_long {
+    exit.update(.any, 0, args.ret) catch {};
     return 0;
 }
--- src/tests/fentry.zig
+++ src/tests/fentry.zig
@@ -25,32 +25,39 @@ test "fentry" {
         return error.LOAD;
     }
 
-    const entry_prog = libbpf.bpf_object__find_program_by_name(obj, "fentry").?;
-    const exit_prog = libbpf.bpf_object__find_program_by_name(obj, "fexit").?;
+    const entry_prog = libbpf.bpf_object__find_program_by_name(obj, "test_fentry").?;
+    const exit_prog = libbpf.bpf_object__find_program_by_name(obj, "test_fexit").?;
 
-    const map = libbpf.bpf_object__find_map_by_name(obj, "ret").?;
+    const arg2 = libbpf.bpf_object__find_map_by_name(obj, "entry").?;
+    const rc = libbpf.bpf_object__find_map_by_name(obj, "exit").?;
 
     const entry_link = libbpf.bpf_program__attach(entry_prog) orelse {
         print("failed to attach entry_prog {s}: {}\n", .{ libbpf.bpf_program__name(entry_prog), std.os.errno(-1) });
         return error.ATTACH;
     };
-    defer _ = libbpf.bpf_link__detach(entry_link);
+    defer _ = libbpf.bpf_link__destroy(entry_link);
     const exit_link = libbpf.bpf_program__attach(exit_prog) orelse {
         print("failed to attach prog {s}: {}\n", .{ libbpf.bpf_program__name(exit_prog), std.os.errno(-1) });
         return error.ATTACH;
     };
-    defer _ = libbpf.bpf_link__detach(exit_link);
+    defer _ = libbpf.bpf_link__destroy(exit_link);
 
     var buf: [64]u8 = undefined;
     const n = std.os.linux.listxattr("/nonexist", &buf, buf.len);
-    const expected = buf.len +| n;
 
     const k: u32 = 0;
-    var got: usize = undefined;
-    ret = libbpf.bpf_map__lookup_elem(map, &k, @sizeOf(@TypeOf(k)), &got, @sizeOf(@TypeOf(got)), 0);
+    var arg2_got: usize = undefined;
+    ret = libbpf.bpf_map__lookup_elem(arg2, &k, @sizeOf(@TypeOf(k)), &arg2_got, @sizeOf(@TypeOf(arg2_got)), 0);
     if (ret != 0) {
         print("failed loopup map element: {}\n", .{std.os.errno(-1)});
         return error.MAP_LOOKUP;
     }
-    try testing.expectEqual(expected, got);
+    var rc_got: usize = undefined;
+    ret = libbpf.bpf_map__lookup_elem(rc, &k, @sizeOf(@TypeOf(k)), &rc_got, @sizeOf(@TypeOf(rc_got)), 0);
+    if (ret != 0) {
+        print("failed loopup map element: {}\n", .{std.os.errno(-1)});
+        return error.MAP_LOOKUP;
+    }
+    try testing.expectEqual(buf.len, arg2_got);
+    try testing.expectEqual(n, rc_got);
 }
