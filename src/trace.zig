e7593e529970759f108669095867771c19834fa1
Tw <tw19881113@gmail.com>
2023-11-10 20:00:47 +0800

    tools/trace: support loop timeout
    Signed-off-by: Tw <tw19881113@gmail.com>

--- src/trace.zig
+++ src/trace.zig
@@ -9,11 +9,26 @@ const TRACE_RECORD = bpf.Args.TRACE_RECORD;
 const hasFn = std.meta.trait.hasFn;
 const is_pointer = bpf.Args.is_pointer;
 const cast = bpf.Args.cast;
+const process = std.process;
 
 const kprobes = @import("build_options").kprobes;
 const syscalls = @import("build_options").syscalls;
 
 var exiting = false;
+
+fn usage() void {
+    print(
+        \\ Usage:
+        \\ --timeout [seconds]
+        \\ --help
+    ++ "\n", .{});
+}
+
+fn nextArg(args: [][]const u8, idx: *usize) ?[]const u8 {
+    if (idx.* >= args.len) return null;
+    defer idx.* += 1;
+    return args[idx.*];
+}
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -21,9 +36,31 @@ pub fn main() !void {
 
     const allocator = arena.allocator();
 
+    var args = try process.argsAlloc(allocator);
+    defer process.argsFree(allocator, args);
+    var arg_idx: usize = 1; // skip exe name
+
     const obj_bytes = @embedFile("@bpf_prog");
     const bytes = try allocator.dupe(u8, obj_bytes);
     defer allocator.free(bytes);
+
+    var seconds: ?usize = null;
+    while (nextArg(args, &arg_idx)) |arg| {
+        if (std.mem.eql(u8, arg, "--timeout")) {
+            const s = nextArg(args, &arg_idx) orelse {
+                usage();
+                return error.ARGS;
+            };
+            seconds = try std.fmt.parseUnsigned(usize, s, 0);
+        } else if (std.mem.eql(u8, arg, "--help")) {
+            usage();
+            return;
+        } else {
+            print("unknown parameter\n", .{});
+            usage();
+            return error.ARGS;
+        }
+    }
 
     const obj = libbpf.bpf_object__open_mem(bytes.ptr, bytes.len, null);
     if (obj == null) {
@@ -64,11 +101,17 @@ pub fn main() !void {
     defer libbpf.ring_buffer__free(ring_buf);
 
     try setup_ctrl_c();
+    const begin_ts = std.time.timestamp();
     while (!exiting) {
         ret = libbpf.ring_buffer__poll(ring_buf, 100);
 
         if (ret < 0) {
-            break;
+            return error.POLL;
+        }
+
+        if (seconds) |timeout| {
+            const cur_ts = std.time.timestamp();
+            if (cur_ts - begin_ts > timeout) break;
         }
     }
 }
