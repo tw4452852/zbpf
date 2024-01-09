3713e1bc2a0eed17b038adef56d4b474cb8ecec3
Tw <tw19881113@gmail.com>
2024-01-09 11:26:31 +0800

    adapt to the new zig build system API
    Due to this change:
    https://github.com/ziglang/zig/commit/142471fcc46070326526e3976f0150fe734df0b6#diff-62679460c0f408cf55ccda6e2ef45dc5a7a0b11185448f5b5dcdaa96f8a28d30
    
    Signed-off-by: Tw <tw19881113@gmail.com>

--- build.zig
+++ build.zig
@@ -1,9 +1,8 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const fs = std.fs;
-const Builder = std.build.Builder;
 
-fn create_bpf_prog(ctx: *const Ctx, src_path: []const u8) *std.build.CompileStep {
+fn create_bpf_prog(ctx: *const Ctx, src_path: []const u8) *std.Build.Step.Compile {
     const name = fs.path.stem(src_path);
 
     const prog = ctx.b.addObject(.{
@@ -11,23 +10,24 @@ fn create_bpf_prog(ctx: *const Ctx, src_path: []const u8) *std.build.CompileStep
         .root_source_file = .{
             .path = src_path,
         },
-        .target = .{
-            .cpu_arch = switch ((ctx.target.cpu_arch orelse builtin.cpu.arch).endian()) {
+        .target = ctx.b.resolveTargetQuery(.{
+            .cpu_arch = switch (ctx.target.result.cpu.arch.endian()) {
                 .big => .bpfeb,
                 .little => .bpfel,
             },
             .os_tag = .freestanding,
-        },
+        }),
         .optimize = .ReleaseFast,
     });
-    prog.addModule("bpf", ctx.bpf);
-    prog.addModule("build_options", ctx.bpf);
-    prog.addOptions("build_options", ctx.build_options);
+    prog.root_module.strip = false;
+    prog.root_module.addImport("bpf", ctx.bpf);
+    prog.root_module.addImport("build_options", ctx.bpf);
+    prog.root_module.addOptions("build_options", ctx.build_options);
 
     return prog;
 }
 
-fn create_libbpf(b: *Builder, target: std.zig.CrossTarget, optimize: std.builtin.Mode) *std.build.CompileStep {
+fn create_libbpf(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
     return b.dependency("libbpf", .{
         .target = target,
         .optimize = optimize,
@@ -35,10 +35,10 @@ fn create_libbpf(b: *Builder, target: std.zig.CrossTarget, optimize: std.builtin
     }).artifact("bpf");
 }
 
-fn create_vmlinux(b: *Builder) *Builder.Module {
+fn create_vmlinux(b: *std.Build) *std.Build.Module {
     // build for native
-    const target = std.zig.CrossTarget{};
-    const optimize: std.builtin.Mode = .ReleaseFast;
+    const target = b.host;
+    const optimize: std.builtin.OptimizeMode = .ReleaseFast;
 
     const libbpf = create_libbpf(b, target, optimize);
     const exe = b.addExecutable(.{
@@ -63,27 +63,27 @@ fn create_vmlinux(b: *Builder) *Builder.Module {
     });
     zigify.addIncludeDir("src/vmlinux_dumper");
     zigify.step.dependOn(&vmlinux_h.step);
-    return b.addModule("vmlinux", .{ .source_file = .{ .generated = &zigify.output_file } });
+    return b.addModule("vmlinux", .{ .root_source_file = .{ .generated = &zigify.output_file } });
 }
 
-fn create_bpf(b: *Builder, vmlinux: *Builder.Module) *Builder.Module {
+fn create_bpf(b: *std.Build, vmlinux: *std.Build.Module) *std.Build.Module {
     return b.addModule("bpf", .{
-        .source_file = .{ .path = "src/bpf/root.zig" },
-        .dependencies = &.{.{ .name = "vmlinux", .module = vmlinux }},
+        .root_source_file = .{ .path = "src/bpf/root.zig" },
+        .imports = &.{.{ .name = "vmlinux", .module = vmlinux }},
     });
 }
 
 const Ctx = struct {
-    b: *Builder,
-    target: std.zig.CrossTarget,
-    optimize: std.builtin.Mode,
-    vmlinux: *Builder.Module,
-    bpf: *Builder.Module,
-    libbpf_step: *std.build.CompileStep,
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    vmlinux: *std.Build.Module,
+    bpf: *std.Build.Module,
+    libbpf_step: *std.Build.Step.Compile,
     build_options: *std.Build.Step.Options,
 };
 
-pub fn build(b: *Builder) !void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -132,12 +132,12 @@ fn create_target_step(ctx: *const Ctx, main_path: []const u8, prog_path: []const
         .target = ctx.target,
         .optimize = ctx.optimize,
     });
-    exe.addAnonymousModule("@bpf_prog", .{
-        .source_file = prog.getOutputSource(),
+    exe.root_module.addAnonymousImport("@bpf_prog", .{
+        .root_source_file = prog.getEmittedBin(),
     });
-    exe.addModule("bpf", ctx.bpf);
-    exe.addModule("vmlinux", ctx.vmlinux);
-    exe.addOptions("build_options", ctx.build_options);
+    exe.root_module.addImport("bpf", ctx.bpf);
+    exe.root_module.addImport("vmlinux", ctx.vmlinux);
+    exe.root_module.addOptions("build_options", ctx.build_options);
 
     exe.linkLibrary(ctx.libbpf_step);
     exe.linkLibC();
@@ -164,7 +164,7 @@ fn create_test_step(ctx: *const Ctx) !void {
     });
     exe_tests.filter = ctx.b.option([]const u8, "test", "test filter");
     exe_tests.linkLibrary(ctx.libbpf_step);
-    exe_tests.addModule("bpf", ctx.bpf);
+    exe_tests.root_module.addImport("bpf", ctx.bpf);
     exe_tests.linkLibC();
     const install_test = ctx.b.addInstallArtifact(exe_tests, .{});
 
@@ -174,13 +174,13 @@ fn create_test_step(ctx: *const Ctx) !void {
     var it = sample_dir.iterate();
     while (try it.next()) |entry| {
         const bpf_prog = create_bpf_prog(ctx, try fs.path.join(ctx.b.allocator, &[_][]const u8{ "samples", entry.name }));
-        exe_tests.addAnonymousModule(try std.fmt.allocPrint(ctx.b.allocator, "@{s}", .{fs.path.stem(entry.name)}), .{
-            .source_file = bpf_prog.getOutputSource(),
+        exe_tests.root_module.addAnonymousImport(try std.fmt.allocPrint(ctx.b.allocator, "@{s}", .{fs.path.stem(entry.name)}), .{
+            .root_source_file = bpf_prog.getEmittedBin(),
         });
     }
 
     // add debug option to test
-    exe_tests.addOptions("build_options", ctx.build_options);
+    exe_tests.root_module.addOptions("build_options", ctx.build_options);
 
     const build_test_step = ctx.b.step("test", "Build unit tests");
     build_test_step.dependOn(&install_test.step);
@@ -194,9 +194,9 @@ fn create_docs_step(ctx: *const Ctx) !void {
         .optimize = ctx.optimize,
     });
 
-    const dumb_vmlinux = ctx.b.addModule("dumb_vmlinux", .{ .source_file = .{ .path = "src/docs/dummy_vmlinux.zig" } });
+    const dumb_vmlinux = ctx.b.addModule("dumb_vmlinux", .{ .root_source_file = .{ .path = "src/docs/dummy_vmlinux.zig" } });
     const bpf = create_bpf(ctx.b, dumb_vmlinux);
-    exe.addModule("bpf", bpf);
+    exe.root_module.addImport("bpf", bpf);
 
     const install_docs = ctx.b.addInstallDirectory(.{
         .source_dir = exe.getEmittedDocs(),
